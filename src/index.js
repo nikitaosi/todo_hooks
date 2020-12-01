80b185e40decd0459e070bae34b9e71a61e3f7b9
Nikita Osipov <nikitaosipov.51@gmail.com>
2020-12-01 21:40:59 +0300

    todo list is finished

--- src/index.js
+++ src/index.js
@@ -3,14 +3,32 @@ import ReactDOM from 'react-dom';
 import Typography from '@material-ui/core/Typography';
 import './styles.css';
 
+import TodoForm from "./TodoForm";
+import TodoList from "./TodoList";
+import useTodoState from './useTodoState';
+
 const App = () => {
-  return (
-    <div className="App">
-      <Typography component="h1" variant="h2">
-        Todos
-      </Typography>
-    </div>
-  );
+    const { todos, addTodo, deleteTodo } = useTodoState([]);
+    return (
+        <div className="App">
+        <Typography component="h1" variant="h2">
+            Todos
+        </Typography>
+
+        <TodoForm 
+            saveTodo={todoText => {
+                const trimmedText = todoText.trim();
+                if (trimmedText.length > 0) {
+                    addTodo([trimmedText])
+                }   
+            }}
+        />
+        <TodoList 
+            todos={todos} 
+            deleteTodo={deleteTodo}
+        />
+        </div>
+    );
 };
 
 const rootElement = document.getElementById('root');
